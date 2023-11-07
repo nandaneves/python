b7592c7d65943f109285d0cca4832b34a96c1da1
FERNANDA MIRANDA NEVES <fernanda.2960@aluno.pr.senac.br>
2023-11-07 13:59:31 -0300

    oiii veh

---
+++ maça.py
@@ -0,0 +1,10 @@
+opcao = int(input('escolha uma opcao: \n1 - maçã\n2 - banana\n3 laranja\nOpção '))
+
+if opcao == 1:
+    print ('voce escolheu Maçã.')
+elif opcao == 2:
+    print('voce escolheu banana.')
+elif opcao == 3:
+    print('voce esolheu laranja')
+else:
+    print('opção inválida')
